b007953042ef3c17502df37ca1db1f9d8422371d
QQ652276536 <652276536@qq.com>
2021-03-11 14:27:17 +0800

    加入了路线规划

--- miniprogram/pages/liwei/map/map.js
+++ miniprogram/pages/liwei/map/map.js
@@ -3,6 +3,19 @@
 var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
 var _mapSdk;
 var _address = 'Undefined';
+var _that;
+var _points = [{
+  latitude: "22.538781",
+  longitude: "113.939431"
+},
+{
+  latitude: "22.529396",
+  longitude: "113.938068"
+},
+{
+  latitude: "22.527696",
+  longitude: "113.929910"
+}]
 
 Page({
 
@@ -11,30 +24,99 @@ Page({
    */
   data: {
     markers: [],
-    lot: '',
-    lat: '',
+    longitude: '',
+    latitude: '',
     txt: {},
     update_flag: false,
+    polyline: []
+  },
+
+  /**
+   * 查询历史位置记录
+   */
+  QueryHistoryLocation() {
+    wx.request({
+      url: 'http://101.132.102.203:8080/GPRS_Web/Location/FindAll',
+      header: { 'content-type': 'application/json' },
+      method: 'POST',
+      timeout: 10 * 1000,
+      success: (result) => {
+        let len = result.data.length;
+        console.log('轨迹记录数：', len);
+        // for (let i = 0; i < len; i++) {
+        //   _points.push({
+        //     latitude: result.data[i].latitude,
+        //     longitude: result.data[i].longitude
+        //   });
+        // }
+
+        console.log('_points：');
+        console.log(_points);
+
+        _mapSdk.direction({
+          made: 'driving',
+          from: '22.538781,113.939431',
+          to: '22.529396,113.938068',
+          success: function (res) {
+            console.log(res);
+            var ret = res;
+            var coors = ret.result.routes[0].polyline, pl = [];
+            //坐标解压（返回的点串坐标，通过前向差分进行压缩）
+            var kr = 1000000;
+            for (var i = 2; i < coors.length; i++) {
+              coors[i] = Number(coors[i - 2]) + Number(coors[i]) / kr;
+            }
+            //将解压后的坐标放入点串数组pl中
+            for (var i = 0; i < coors.length; i += 2) {
+              pl.push({ latitude: coors[i], longitude: coors[i + 1] })
+            }
+            console.log(pl)
+            //设置polyline属性，将路线显示出来,将解压坐标第一个数据作为起点
+            _that.setData({
+              latitude: pl[0].latitude,
+              longitude: pl[0].longitude,
+              polyline: [{
+                points: pl,
+                color: '#FF0000DD',
+                width: 4
+              }],
+              update_flag: true
+            })
+          },
+          fail: function (error) {
+            console.error(error);
+          },
+          complete: function (res) {
+            console.log(res);
+          }
+        })
+
+
+      },
+      fail: (result) => {
+      },
+      complete: (result) => {
+      },
+    })
   },
 
   /**
    * 获取当前位置
    */
-  GetLocation() {
-    var that = this;
+  QueryCurrentLocation() {
     wx.getLocation({
       type: 'gcj02',
       success: (result) => {
         console.log('获取当前位置成功：', result);
-        that.setData({
-          lot: result.longitude,
-          lat: result.latitude
+        _that.setData({
+          longitude: result.longitude,
+          latitude: result.latitude
         })
         //坐标反查
         _mapSdk.reverseGeocoder({
           location: {
-            latitude: this.data.lat,
-            longitude: this.data.lot
+            latitude: this.data.latitude,
+            longitude: this.data.longitude
           },
           //网络请求为异步操作，在请求执行完毕的回调中赋值
           success: function (result) {
@@ -43,8 +125,8 @@ Page({
             console.log('位置：', _address);
             //地图标记
             var marker = [{
-              longitude: that.data.lot,
-              latitude: that.data.lat,
+              longitude: _that.data.longitude,
+              latitude: _that.data.latitude,
               iconPath: '../../liwei/map/mark.png',
               //自定义气泡
               callout: {
@@ -59,7 +141,7 @@ Page({
                 display: 'BYCLICK'
               }
             }]
-            that.setData({
+            _that.setData({
               markers: marker,
               update_flag: true
             });
@@ -82,13 +164,17 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    _that = this;
     var mac = options.mac;
     console.log('收到的设备的MAC地址：' + mac);
     //腾讯地图API核心类
     _mapSdk = new QQMapWX({
       key: 'YOWBZ-ZSKKW-DPKRE-O2OUU-57RC5-NVFCW'
     });
-    this.GetLocation();
+    //查询本机当前位置
+    // this.QueryCurrentLocation();
+    //查询该设备的历史轨迹
+    this.QueryHistoryLocation();
   },
 
   /**
